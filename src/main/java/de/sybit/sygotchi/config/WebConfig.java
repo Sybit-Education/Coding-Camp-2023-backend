d3ba2dc29a344a1cde3d12ce4cdd41db7f198000
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2023-08-07 12:13:29 +0200

    chore: add options

--- src/main/java/de/sybit/sygotchi/config/WebConfig.java
+++ src/main/java/de/sybit/sygotchi/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
